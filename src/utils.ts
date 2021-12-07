ef7aa6e64a2e3c63aa3c80a30a3d87849eccf85c
Sana Lee <sana@sanas-mbp.lan>
2021-12-07 02:52:21 -0800

    conver utils to ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 /* from https://dev.to/jorik/country-code-to-flag-emoji-a21 */
-function getFlagEmoji(countryCode) {
+function getFlagEmoji(countryCode: string) {
     return countryCode.toUpperCase()
-        .replace(/./g, char => String.fromCodePoint(127397 + char.charCodeAt())
+        .replace(/./g, char => String.fromCodePoint(127397 + char.charCodeAt(0))
         );
 }
 
