60e172fb6dd8e60f0fc155fdec6f62ac3ef50ef4
sana-malik <sana-malik@users.noreply.github.com>
2021-12-08 12:08:30 -0800

    Revert theme change

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Flex, Provider } from '@adobe/react-spectrum';
+import { defaultTheme, Flex, Provider } from '@adobe/react-spectrum';
 import Header from './components/Header';
 import Content from './components/Content';
 import './css/App.css';
 
 function App() {
   return (
-    <Provider theme="light" height="100%">
+    <Provider theme={defaultTheme} height="100%">
       <Flex direction="column" height="100%">
         <Header />
         <Content />
