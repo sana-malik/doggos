beee3ee0fc8c622d9e26b1d50cfb813c356db65b
sana-malik <sana-malik@users.noreply.github.com>
2021-12-07 20:17:48 -0800

    Update App.tsx
    Force light theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { defaultTheme, Flex, Provider } from '@adobe/react-spectrum';
+import { Flex, Provider } from '@adobe/react-spectrum';
 import Header from './components/Header';
 import Content from './components/Content';
 import './css/App.css';
 
 function App() {
   return (
-    <Provider theme={defaultTheme} height="100%">
+    <Provider theme="light" height="100%">
       <Flex direction="column" height="100%">
         <Header />
         <Content />
